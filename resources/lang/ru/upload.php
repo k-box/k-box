dee25b75095676ddee32c854c575b21cdc37fe33
salaidin <salaidin@oneofftech.xyz>
2017-11-09 15:52:20 +0600

    Second part of upload.php translations

--- resources/lang/ru/upload.php
+++ resources/lang/ru/upload.php
@@ -18,11 +18,26 @@ return [
     'cancel_question' => 'Вы действительно хотите прервать загрузку?',
 
     'action_drop' => 'Переместите файлы сюда для начала загрузки',
-    'action_select' => 'Выбрать',
+    'action_select' => 'Выбрать файлы',
 
+    'to' => 'в',
 
+    'do_not_leave_the_page' => 'Пожалуйста, не закрывайте эту страницу до заершения процесса загрузки. Вы можете продолжать использовать ваш брузер открыв новую вкладку.',
 
+    'target' => [
+    'personal' => 'Загрузить в ваши <a href=":link" target="_blank" rel="noopener noreferrer">личные</a> файлы.',
+    'collection' => 'Загрузить в коллекцию <a href=":link" target="_blank" rel="noopener noreferrer">:name</a> в <strong>Моих коллекциях</strong>.',
+    'project' => 'Загрузить в проект <a href=":link" target="_blank" rel="noopener noreferrer">:name</a>.',
+    'project_collection' => 'Загрузить в коллекцию <a href=":link" target="_blank" rel="noopener noreferrer">:name</a> в проекте <a href=":project_link" target="_blank" rel="noopener noreferrer">:project_name</a>.',
+    'error' => 'У вас нет доступа к данной коллекции',
+    ],
 
     'status' => [
+        'started' => 'Начало загрузки',
+        'queued' => 'В очереди загрузок',
+        'uploading' => 'Загрузка файлов',
+        'completed' => 'Загрузка завершена',
+        'cancelled' => 'Загрузка отменена',
+        'failed' => 'Ошибка при загрузке',
     ],
 ];
