5d3fbb1794ddd378a7fa2ff2e7048b72032987f5
shenriod <s@henriod.info>
2017-12-20 23:12:26 +0100

    Translation into French

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -9,7 +9,7 @@ return [
     |
     | The following language lines are used during authentication for various
     | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
+    | these language lines according to your application's requirements. 
     |
     */
 
