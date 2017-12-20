fffdec02c0f20a9c13d5a057ceb20156f1ef3d32
shenriod <s@henriod.info>
2017-12-20 23:08:53 +0100

    Update auth.php
    First round of translation into French

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Ces informations de connexion ne correspondent pas à nos enregistrements.',
+    'throttle' => 'Trop de tentatives de connexions. Merci de réessayer dans :seconds secondes.',
 
 ];
