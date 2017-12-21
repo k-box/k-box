2b9c4964398b910e84a98986f66b8c52ed7a9665
shenriod <s@henriod.info>
2017-12-21 23:23:25 +0100

    Translation into French

--- resources/lang/fr/pages.php
+++ resources/lang/fr/pages.php
@@ -11,13 +11,13 @@ return [
     |
     */
 
-    'help' => 'Help',
+    'help' => 'Aide',
     'support' => 'Support',
-    'terms'  => 'Terms',
-    'terms_long'  => 'Terms of Service',
-    'privacy' => 'Privacy',
+    'terms'  => 'Conditions',
+    'terms_long'  => 'Conditions d\'utilisation',
+    'privacy' => 'Confidentialité',
     'contact' => 'Contact',
-    'service_policy' => 'Service Policy',
-    'browserupdate' => 'Browser Update Help',
+    'service_policy' => 'Politique de service',
+    'browserupdate' => 'Aide mise à jour du navigateur',
 
 ];
