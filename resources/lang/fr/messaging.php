e36b09662590cc4cd396737ab7762778327c2d05
shenriod <s@henriod.info>
2017-12-21 23:04:43 +0100

    Translation into French

--- resources/lang/fr/messaging.php
+++ resources/lang/fr/messaging.php
@@ -11,28 +11,28 @@ return [
     |
     */
 
-    'create_pagetitle' => 'Create new Message to...',
+    'create_pagetitle' => 'Créer un nouveau message pour...',
     
-    'message_sent' => 'The message has been sent.',
-    'message_error' => 'There was an error sending the message. :error',
-    'error_empty_users' => 'Please select at least one user.',
-    'error_users_not_found' => 'The following recipients cannot be found: :users',
+    'message_sent' => 'Le message a été envoyé.',
+    'message_error' => 'Il y a eu une erreur pendant l\'envoi du message. :error',
+    'error_empty_users' => 'Veuillez sélectionner au moins un utilisateur.',
+    'error_users_not_found' => 'Les destinataires suivant ne peuvent pas être trouvés: :users',
 
     'labels' => [
-        'users' => 'Select the users that will receive the message',
-        'text' => 'Insert the text of the message',
-        'submit_btn' => 'Send Message',
+        'users' => 'Sélectionner les utilisateurs qui recevront le message',
+        'text' => 'Insérer le texte du message',
+        'submit_btn' => 'Envoyer le message',
     ],
 
     'mail' => [
 
-        'intro' => 'Dear :name,',
+        'intro' => 'Cher :name,',
         
-        'subject' => 'Message from K-Box',
+        'subject' => 'Message de K-Box',
         
-        'signature' => ':name<br/>Sent from K-Box.',
+        'signature' => ':name<br/>Envoyé depuis K-Box.',
         
-        'you_are_receiving_because' => 'You are receiving this e-mail because you are a user of the <a href=":link">K-Box</a>.',
+        'you_are_receiving_because' => 'Vous recevez cet email parce que vous êtes un utilisateur de <a href=":link">K-Box</a>.',
         
         
     ],
