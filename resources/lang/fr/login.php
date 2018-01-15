cbdac98ca4db3150b5fe08b3ebd88354ef569ede
shenriod <s@henriod.info>
2018-01-15 22:14:11 +0100

    Translation into French [double-check]
    Translation into French [double-check]

--- resources/lang/fr/login.php
+++ resources/lang/fr/login.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Login Language Lines
+    | Login Language Lines 
     |--------------------------------------------------------------------------
     |
     | The following language lines are used for the login box and page
