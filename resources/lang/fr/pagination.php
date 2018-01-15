d0b15c36d58c03b143b7714aa8e12e75c479b6cc
shenriod <s@henriod.info>
2018-01-15 22:23:51 +0100

    Translation into French [double-check]
    Translation into French [double-check]

--- resources/lang/fr/pagination.php
+++ resources/lang/fr/pagination.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Pagination Language Lines
+    | Pagination Language Lines 
     |--------------------------------------------------------------------------
     |
     | The following language lines are used by the paginator library to build
