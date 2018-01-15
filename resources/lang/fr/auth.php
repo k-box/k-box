2269f657af720734a181336d80c3aca732cfea87
shenriod <s@henriod.info>
2018-01-15 21:50:38 +0100

    Translation into French [double-check]
    Translation into French [double-check]

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Language Lines
+    | Authentication Language Lines 
     |--------------------------------------------------------------------------
     |
     | The following language lines are used during authentication for various
