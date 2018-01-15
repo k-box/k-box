7007d76059a6714d530c20bf1fe30ffd4d0ecebf
shenriod <s@henriod.info>
2018-01-15 22:30:40 +0100

    Translation into French [double-check]
    Translation into French [double-check]

--- resources/lang/fr/projects.php
+++ resources/lang/fr/projects.php
@@ -27,11 +27,11 @@ return [
     'labels' => [
         'name' => 'Nom du projet',
         'description' => 'Description du projet',
-        'project_details' => 'Details du projet',
+        'project_details' => 'Détails du projet',
         
         'users' => 'Utilisateurs',
         'search_member_placeholder' => 'Rechercher un membre du projet...',
-        'search_member_not_found' => 'Aucun membre avec ce nom ou cette institution.',
+        'search_member_not_found' => 'Aucun membre avec ce nom ou cette institution n\'a été trouvé.',
         'add_users' => 'Ajouter des utilisateurs au projet',
         'add_users_button' => 'Ajouter un utilisateur',
         'users_placeholder' => 'Sélectionner un / des utilisateur(s)',
@@ -65,7 +65,7 @@ return [
 
     'no_user_available' => 'Aucun utilisateur enregistré ne peut être ajouté au projet. Il est possible que vous ayiez déjà ajouté tous les utilisateurs.',
     
-    'no_members' => 'Il n\'y a pas encore d\'utilisateurs dans ce projet. Commencez pas ajouter quelqu\'un.',
+    'no_members' => 'Il n\'y a pas encore d\'utilisateurs dans ce projet. Commencez par ajouter quelqu\'un.',
 
     'empty_selection' => 'Sélectionnez un projet pour voir les détails',
     'empty_projects' => 'Aucun projet. <a href=":url">Créez</a> un nouveau projet',
