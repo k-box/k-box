1a1db8e6475d91cfdbebf0b57a920e42325ff8bb
shenriod <s@henriod.info>
2018-02-05 10:49:20 +0500

    Update help.php

--- packages/licenses/lang/fr/help.php
+++ packages/licenses/lang/fr/help.php
@@ -4,6 +4,6 @@ return [
 
     'description_disclaimer' => 'Cet acte met uniquement en évidence quelques unes des caractéristiques principales et des conditions de cette licence. Ce n\'est pas une licence et n\'a pas de valeur légale. Vous devriez minutieusement passer en revue les termes et conditions de la licence avant d\'utiliser une œuvre sous licence. K-Link et la K-Box ne sont pas un cabinet d\'avocats et ne fournissent pas de conseils légaux.',
 
-    'deed_intro' => 'C\'est un résumé (et non pas un substitut) de la <a href=":link">licence</a>.'
+    'deed_intro' => 'Ceci est un résumé (et non pas un substitut) de la <a href=":link">licence</a>.'
 
 ];
