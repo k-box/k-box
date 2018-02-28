ebd7123a5c771f8e86bc52e7a487dbf9ad4e7297
Alessio <alessio.vertemati@gmail.com>
2018-02-28 20:28:32 +0100

    Typo fixes in licenses description disclaimer text

--- packages/licenses/lang/en/help.php
+++ packages/licenses/lang/en/help.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'description_disclaimer' => 'This summary highlights only some of the key features and terms of the actual license. It is not a license and has no legal value. You should carefully review all of the terms and conditions of the actual license before using the licensed material. K-Link and the K-Box are not a law firm and does not provide legal services.',
+    'description_disclaimer' => 'This summary highlights only some of the key features and terms of the actual license. It is not a license and has no legal value. You should carefully review all of the terms and conditions of the actual license before using the licensed material. K-Link and K-Box are not a law firm and do not provide legal services.',
 
     'deed_intro' => 'This is a human-readable summary of (and not a substitute for) the <a href=":link" rel="noopener noreferrer nofollow">license</a>.'
 
