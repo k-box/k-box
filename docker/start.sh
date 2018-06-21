b1a9279763f647cd18a779e98243c77a43375a43
Alessio <alessio.vertemati@gmail.com>
2018-06-21 14:28:51 +0200

    Remove useless switch and variable

--- docker/start.sh
+++ docker/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-COMMAND=${1}
-
 echo "K-Box start in progress..."
 
-case "${COMMAND}" in
-    *)
-        /usr/local/bin/configure.sh && exec /usr/bin/supervisord
-   ;;
-esac
+/usr/local/bin/configure.sh && exec /usr/bin/supervisord
