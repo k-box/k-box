8629b0dbcfc6bd9aaaf56b83b32ee09b50c10c00
Alessio <avvertix@users.noreply.github.com>
2018-06-26 19:04:39 +0200

    Fix trash and delete of collection hierarchy (#101)
    - Fix collection trashing and deletion when collections are in a project.
    - Modify trash policy: all users can trash a project collection, but only the creator or the project manager can permanently delete it
    - Sub-collections are always trashed if the containing collection is trashed.

--- resources/lang/en/groups.php
+++ resources/lang/en/groups.php
@@ -109,8 +109,12 @@ return [
         
         'cannot_delete_general_error' => 'Cannot delete the specified elements. Nothing has been deleted.',
         
+        'forbidden_trash_personal_collection' => 'You did not create :collection, therefore you cannot trash it.',
+        'forbidden_delete_personal_collection' => 'You did not create :collection, therefore you cannot delete it.',
         'forbidden_delete_collection' => 'The collection :collection cannot be deleted. You are not allowed to operate on Collections.',
         'forbidden_delete_project_collection' => 'The collection :collection cannot be deleted as it is in a project where you do not have the edit permissions.',
+        'forbidden_delete_project_collection_not_creator' => 'You are not the creator of the collection :collection, therefore you cannot delete it.',
+        'forbidden_delete_project_collection_not_manager' => 'You are not the manager of the project that contained :collection, therefore you cannot delete it.',
     ],
     
     'move' => [
