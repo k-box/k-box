8c9e5dac6761538b5ca094dd906513e4815acbe6
AlbinaMuzafarova <36408342+AlbinaMuzafarova@users.noreply.github.com>
2018-07-04 19:11:29 +0200

    add do_not_reply to PassReset

--- resources/views/emails/pass-reset-html.blade.php
+++ resources/views/emails/pass-reset-html.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html>
     <body>
+    <p>{{ trans('messaging.mail.do_not_reply) }}</p>
+    
     {{$user->name}}, your new password is <br/>
     <strong>{{$password}}</strong>
     </body>
