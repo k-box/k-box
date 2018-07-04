3665cd8a2fdc4c696f2bfe3a597e2eecf6d7a168
AlbinaMuzafarova <36408342+AlbinaMuzafarova@users.noreply.github.com>
2018-07-04 19:20:38 +0200

    moved to the bottom

--- resources/views/emails/message-html.blade.php
+++ resources/views/emails/message-html.blade.php
@@ -4,8 +4,6 @@
 
     <img src="{{ $message->embed(public_path('images/klink_mail_logo.png')) }}" alt="K-Link">
     
-    <p>{{ trans('messaging.mail.do_not_reply) }}</p>
-
     <p>{{ trans('messaging.mail.intro', ['name' => $user['name']]) }}</p>
 
     {!! $text !!}
@@ -18,6 +16,9 @@
     <p>
     	{!! trans('messaging.mail.you_are_receiving_because', ['link' => \Config::get('app.url')]) !!}
     </p>
+    
+    <p>{{ trans('messaging.mail.do_not_reply) }}</p>
+
     
     </body>
 </html>
