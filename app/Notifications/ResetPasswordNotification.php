f4f53a0664fc5b6a6949e520dee84e4189c90bbf
Alessio <alessio.vertemati@gmail.com>
2018-07-05 11:31:49 +0200

    Add salutation message to email messages

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -50,6 +50,7 @@ class ResetPasswordNotification extends Notification
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
             ->action('Reset Password', url('password/reset', $this->token))
+            ->salutation(trans('messaging.mail.do_not_reply'))
             ->line('If you did not request a password reset, no further action is required.');
     }
 }
