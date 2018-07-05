ec92a42e396eb347095ad4df085da43f5d9242d7
Alessio <alessio.vertemati@gmail.com>
2018-07-05 20:55:27 +0200

    Preliminary thumbnail and download controllers

---
+++ app/Http/Controllers/Document/DocumentDownloadController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KBox\Http\Controllers\Document;
+
+use Illuminate\Http\Request;
+use KBox\Http\Controllers\Controller;
+
+class DocumentDownloadController extends Controller
+{
+    public function show()
+    {
+        
+    }
+}
---
+++ app/Http/Controllers/Document/DocumentThumbnailController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KBox\Http\Controllers\Document;
+
+use Illuminate\Http\Request;
+use KBox\Http\Controllers\Controller;
+
+class DocumentThumbnailController extends Controller
+{
+    public function show()
+    {
+        
+    }
+}
