9d7b4e6bb8aba7581f24ecc43552d577ac84158d
Alessio <alessio.vertemati@gmail.com>
2018-08-28 16:33:36 +0200

    Add file service to interact with files and register new types

--- packages/contentprocessing/src/Services/FileService.php
+++ packages/contentprocessing/src/Services/FileService.php
@@ -2,6 +2,8 @@
 
 namespace KBox\Documents\Services;
 
+use KBox\Documents\FileHelper;
+use Illuminate\Support\Facades\Storage;
 use KBox\Documents\Preview\PreviewFactory;
 
 /**
@@ -12,9 +14,52 @@ use KBox\Documents\Preview\PreviewFactory;
 class FileService
 {
 
-    
-    public function register($mimeType, $documentType, $extension, $previewGenerator, $thumbnailGenerator, $textExtractor)
+    // public function register($mimeType, $documentType, $extension, $previewGenerator, $thumbnailGenerator, $textExtractor)
+    // {
+    //     // probably here I would register the resolver from plugins    
+    // }
+
+
+    /**
+     * Computes the hash of the file content
+     * 
+     * Uses SHA-512 variant of SHA-2 (Secure hash Algorithm)
+     * 
+     * @param string $path The file path
+     * @return string
+     */
+    public function hash(string $path)
     {
-        // qui registro il tipo di file    
+        $absolute_path = @is_file($path) ? $path : Storage::path($path);
+
+        return FileHelper::hash($absolute_path);
     }
+
+
+    /**
+     * Retrieve the mime type and document type of a file, given its path on disk
+     * 
+     * @param string $path The path to the file
+     * @return array with mime type as first element, and document type as second
+     */
+    public function recognize($path)
+    {
+        $absolute_path = @is_file($path) ? $path : Storage::path($path);
+
+        return FileHelper::type($absolute_path);
+    }
+
+    /**
+     * Return the file extension that corresponds to the given mime type and document type
+     * 
+     * @param  string $mimeType the mime-type of the file
+     * @param  string $documentType the document-type of the file. Default null
+     * @return string           the known file extension
+     * @throws InvalidArgumentException If the mime type is unkwnown, null or empty
+     */
+    public static function extensionFromType($mimeType, $documentType = null)
+    {
+        return FileHelper::getExtensionFromType($mimeType, $documentType);
+    }
+
 }
