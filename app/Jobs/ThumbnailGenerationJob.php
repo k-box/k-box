d5d1ee4f82325447cb787c117bf7218de933bebc
Alessio <alessio.vertemati@gmail.com>
2018-09-04 10:12:50 +0200

    Checking why pdf thumbnail test fails on Travis CI

--- app/Jobs/ThumbnailGenerationJob.php
+++ app/Jobs/ThumbnailGenerationJob.php
@@ -56,6 +56,7 @@ class ThumbnailGenerationJob extends Job implements ShouldQueue
         try {
             $service->generate($this->file);
         } catch (Exception $ex) {
+            dump('Thumbnail generation exception', $ex);
             Log::error('Thumbnail generation Job error', [
                 'file' => $this->file->toArray(),
                 'force' => $this->force,
