3dfc93e1d4524c51d55bfaaa6886fdc1fc2bb699
Alessio <alessio.vertemati@gmail.com>
2018-09-11 12:17:44 +0200

    Image preview driver

---
+++ tests/Unit/Documents/PreviewDriver/ImagePreviewDriverTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Unit\Documents\PreviewDriver;
+
+use Tests\TestCase;
+use KBox\File;
+use KBox\DocumentDescriptor;
+use KBox\Documents\FileHelper;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use KBox\Documents\Preview\ImagePreviewDriver;
+use Illuminate\Contracts\Support\Renderable;
+
+class ImagePreviewDriverTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected function createFileForPath($path)
+    {
+        list($mimeType) = FileHelper::type($path);
+
+        return factory(File::class)->create([
+            'path' => $path,
+            'mime_type' => $mimeType
+        ]);
+    }
+    
+    public function test_jpg_can_be_previewed()
+    {
+        $path = __DIR__.'/data/example.jpg';
+
+        $file = $this->createFileForPath($path);
+
+        $document = factory(DocumentDescriptor::class)->create([
+            'file_id' => $file->id,
+            'mime_type' => $file->mime_type
+        ]);
+
+        $preview = (new ImagePreviewDriver())->preview($file);
+        $preview->with(['document' => $document]);
+        $html = $preview->render();
+
+        $this->assertInstanceOf(ImagePreviewDriver::class, $preview);
+        $this->assertInstanceOf(Renderable::class, $preview);
+        $this->assertNotNull($html);
+        $this->assertNotEmpty($html);
+        $this->assertContains('img src', $html);
+        $this->assertContains($document->uuid, $html);
+        $this->assertContains($file->uuid, $html);
+    }
+    
+    public function test_gif_can_be_previewed()
+    {
+        $path = __DIR__.'/data/example.gif';
+
+        $file = $this->createFileForPath($path);
+
+        $document = factory(DocumentDescriptor::class)->create([
+            'file_id' => $file->id,
+            'mime_type' => $file->mime_type
+        ]);
+
+        $preview = (new ImagePreviewDriver())->preview($file);
+        $preview->with(['document' => $document]);
+        $html = $preview->render();
+
+        $this->assertInstanceOf(ImagePreviewDriver::class, $preview);
+        $this->assertInstanceOf(Renderable::class, $preview);
+        $this->assertNotNull($html);
+        $this->assertNotEmpty($html);
+        $this->assertContains('img src', $html);
+        $this->assertContains($document->uuid, $html);
+        $this->assertContains($file->uuid, $html);
+    }
+    
+    public function test_png_can_be_previewed()
+    {
+        $path = __DIR__.'/data/project-avatar.png';
+
+        $file = $this->createFileForPath($path);
+
+        $document = factory(DocumentDescriptor::class)->create([
+            'file_id' => $file->id,
+            'mime_type' => $file->mime_type
+        ]);
+
+        $preview = (new ImagePreviewDriver())->preview($file);
+        $preview->with(['document' => $document]);
+        $html = $preview->render();
+
+        $this->assertInstanceOf(ImagePreviewDriver::class, $preview);
+        $this->assertInstanceOf(Renderable::class, $preview);
+        $this->assertNotNull($html);
+        $this->assertNotEmpty($html);
+        $this->assertContains('img src', $html);
+        $this->assertContains($document->uuid, $html);
+        $this->assertContains($file->uuid, $html);
+    }
+}
