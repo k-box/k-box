d84d48b617027b947d636e8fc19aa8b919d01d7b
salaidin <salaidin@oneofftech.xyz>
2018-09-18 12:06:18 +0600

    Minor chages

--- resources/lang/ky/upload.php
+++ resources/lang/ky/upload.php
@@ -24,11 +24,13 @@ return [
 
     'do_not_leave_the_page' => 'Жүктөө аяктаганга чейин баракчаны жапбай туруңуз. Браузерди колдонуу үчүн жаңы кыстарманы ачыңыз.',
 
+    'upload_spec_info' => 'Видео MP4 форматта H.264 кодек менен, чечими 480x360 жана 1920x1080 пиксел аралыгында болуш керек',
+
     'target' => [
     'personal' => '<a href=":link" target="_blank" rel="noopener noreferrer">жеке</a> файлдарыңызга',
-    'collection' => '<a href=":link" target="_blank" rel="noopener noreferrer">:name</a> в <strong>Моих коллекциях</strong> коллекцияга.',
+    'collection' => '<a href=":link" target="_blank" rel="noopener noreferrer">:name</a> коллекциясына',
     'project' => '<a href=":link" target="_blank" rel="noopener noreferrer">:name</a> долбоорго.',
-    'project_collection' => 'долбоордогу <a href=":project_link" target="_blank" rel="noopener noreferrer">:project_name</a> коллекцияга <a href=":link" target="_blank" rel="noopener noreferrer">:name</a>',
+    'project_collection' => '<a href=":project_link" target="_blank" rel="noopener noreferrer">:project_name</a> долбоордогу <a href=":link" target="_blank" rel="noopener noreferrer">:name</a> коллекцияга',
     'error' => 'Коллекцияны көрө албайсыз',
     ],
 
