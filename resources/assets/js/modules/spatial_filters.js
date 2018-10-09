ab1f25e47667ee7334b9e1bfe934bcc337591739
Alessio <alessio.vertemati@gmail.com>
2018-10-09 11:16:26 +0200

    Correctly set bounds

--- resources/assets/js/modules/spatial_filters.js
+++ resources/assets/js/modules/spatial_filters.js
@@ -27,7 +27,7 @@ define("modules/spatial_filters", ["require", "modernizr", "jquery", "DMS", "mod
             zoom: 4,
             minZoom: 3,
             maxZoom: 20,
-            maxBounds: L.latLngBounds([[-180, 90], [180,-90]]),
+            maxBounds: L.latLngBounds([[90, -180], [-90, 180]]),
             maxBoundsViscosity: 0.8
         })
         
