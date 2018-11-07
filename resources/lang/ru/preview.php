5678a0a283fd920d75096779eea944b8b5a2402a
Alessio <avvertix@users.noreply.github.com>
2018-11-07 10:02:40 +0100

    Update Russian translation (#169)
    * Preview page
    * Search
    * Geographic Extension

---
+++ resources/lang/ru/preview.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'presentation' => [
+        'hidden_slide_message' => 'Скрытый слайд',
+    ],
+
+    'map' => [
+        'lat_lng_location_hint' => 'Широта и долгота точки',
+        'value_label' => 'значение',
+    ],
+
+];
