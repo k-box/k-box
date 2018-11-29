6301910ad98834307970af3c603bc5ca6d52c631
Alessio <avvertix@users.noreply.github.com>
2018-11-29 08:48:07 +0100

    Fix regression when moving collections (#185)
    Moving collection under a different parent in personal or under a project was denied

--- tests/Feature/CollectionControllerTest.php
+++ tests/Feature/CollectionControllerTest.php
@@ -104,4 +104,88 @@ class CollectionControllerTest extends TestCase
             'id' => $collection->id
         ]);
     }
+
+    public function test_move_from_personal_to_personal()
+    {
+        $service = app('KBox\Documents\Services\DocumentsService');
+
+        $user = tap(factory(\KBox\User::class)->create(), function ($u) {
+            $u->addCapabilities(Capability::$PROJECT_MANAGER);
+        });
+        
+        $collection = $service->createGroup($user, 'personal');
+        $collection_under = $service->createGroup($user, 'personal-sub-collection', null, $collection);
+        $collection_under2 = $service->createGroup($user, 'personal-sub-sub-collection', null, $collection_under);
+        
+        $response = $this->actingAs($user)->json('PUT', '/documents/groups/'.$collection_under2->id, [
+            'private' => false,
+            'parent' => $collection->id,
+            'dry_run' => 0,
+            'action' => 'move',
+        ]);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'id' => $collection_under2->id,
+            'parent_id' => $collection->id
+        ]);
+    }
+    
+    public function test_move_from_project_to_project()
+    {
+        $service = app('KBox\Documents\Services\DocumentsService');
+
+        $user = tap(factory(\KBox\User::class)->create(), function ($u) {
+            $u->addCapabilities(Capability::$PROJECT_MANAGER);
+        });
+
+        $project = factory(\KBox\Project::class)->create();
+        
+        $collection = $project->collection;
+        $collection_under = $service->createGroup($user, 'personal-sub-collection', null, $collection, false);
+        $collection_under2 = $service->createGroup($user, 'personal-sub-sub-collection', null, $collection_under, false);
+        
+        $response = $this->actingAs($user)->json('PUT', '/documents/groups/'.$collection_under2->id, [
+            'private' => false,
+            'parent' => $collection->id,
+            'dry_run' => 0,
+            'action' => 'move',
+        ]);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'id' => $collection_under2->id,
+            'parent_id' => $collection->id
+        ]);
+    }
+    
+    public function test_move_from_personal_to_project_cannot_be_perfomed_with_move_action()
+    {
+        $service = app('KBox\Documents\Services\DocumentsService');
+
+        $user = tap(factory(\KBox\User::class)->create(), function ($u) {
+            $u->addCapabilities(Capability::$PROJECT_MANAGER);
+        });
+
+        $project = factory(\KBox\Project::class)->create();
+        
+        $collection = $service->createGroup($user, 'personal');
+        $collection_under = $service->createGroup($user, 'personal-sub-collection', null, $collection);
+        $collection_under2 = $service->createGroup($user, 'personal-sub-sub-collection', null, $collection_under);
+        
+        $response = $this->actingAs($user)->json('PUT', '/documents/groups/'.$collection_under2->id, [
+            'parent' => $project->collection->id,
+            'dry_run' => 0,
+            'action' => 'move',
+        ]);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'id' => $collection_under2->id,
+            'parent_id' => $collection_under->id
+        ]);
+    }
 }
