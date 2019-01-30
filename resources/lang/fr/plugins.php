ac404861dadf255b4c40c8a6ac53a357536bb56a
shenriod <s@henriod.info>
2019-01-30 14:55:56 +0500

    French translation

---
+++ resources/lang/fr/plugins.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Plugins Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used inside the K-Box Plugins 
+    | Administration area
+    |
+    */
+
+    'page_title' => 'Plugins',
+
+    'experimental_notice' => 'Ce plugin est expérimental.',
+
+    'table' => [
+        'name_column' => 'Plugin',
+        'creator_column' => 'Créé par',
+    ],
+
+    'actions' => [
+        'enable' => 'Activer',
+        'disable' => 'Désactiver',
+        'settings' => 'Paramètres du plugin',
+    ],
+
+    'messages' => [
+        'enabled' => 'Plugin activé',
+        'disabled' => 'Plugin désactivé',
+        'settings_saved'=> 'Paramètres du plugin mis à jour',
+    ],
+];
