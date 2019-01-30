279fd2df26949614bedcb73f6c03d332b67633ff
Alessio <alessio.vertemati@gmail.com>
2019-01-30 11:08:53 +0100

    Copy en to fr folder for translation

---
+++ plugins/geo/lang/fr/properties.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Geographic properties localization
+    |--------------------------------------------------------------------------
+    */
+
+    "section" => "Geographic information",
+    
+    "crs" => "Coordinate Reference System",
+    "type" => "Format",
+    "dimension" => "Dimension",
+    "dimension_pixels" => ":width&times;:height pixels",
+    "layers" => "Layers",
+    "geoserver.store" => "Store in GeoServer",
+    
+];
