61e6bc3abd61bd0d6255d553405f3038b5ca7523
Alessio <alessio.vertemati@gmail.com>
2019-04-23 19:18:23 +0200

    Remove verified middleware for now

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -16,7 +16,6 @@ class UserProfileController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('verified');
     }
 
     /**
