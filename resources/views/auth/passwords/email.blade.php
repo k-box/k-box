cf9f7659414ee18548ad02c0fd2b7f393de6d1a4
Alessio <alessio.vertemati@gmail.com>
2019-04-25 10:42:42 +0200

    Update forgot password layout

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,54 +1,45 @@
-@extends('global')
-
-@section('breadcrumbs')
-
-	{{trans('passwords.forgot.title')}}
-
-@stop
+@extends('layout.login')
 
 @push('title')
     @lang('passwords.forgot.title') &ndash; 
 @endpush
 
-@section('content')
+@section('form')
 
-<div class="row">
+	@if(Session::has('status'))
 
-	<div class="twelve columns">
+		<div class="alert success">
+			{{session('status')}}
+		</div>
 
-		@if(Session::has('status'))
+	@endif
 
-			<div class="alert success">
-				{{session('status')}}
-			</div>
+	<form method="post" class="c-form c-form--space" action="{{ route('password.email') }}">
 
-		@endif
-
-
-		<form method="post" action="{{ url('/password/email') }}">
-
-		   {{ csrf_field() }}
-
-		   <p>{{trans('passwords.forgot.instructions')}}</p>
-
-		   <p>
-				<label for="email">{{trans('auth.email_label')}}</label>
-				@if( isset($errors) && $errors->has('email') )
-					<span class="field-error">{{ implode(",", isset($errors) && $errors->get('email') ? $errors->get('email') : [])  }}</span>
-				@endif
-				<input type="email" required id="email" name="email" value="" />
-			</p>
-
-			<p>
-				<button type="submit" class="button-primary">{{trans('passwords.forgot.submit')}}</button>
-			</p>
-
-		</form>
+		<h2 class="mb-1">{{trans('passwords.forgot.title')}}</h2>
 		
+		<p class="mb-4">{{trans('passwords.forgot.instructions')}}</p>
 
-	</div>
-	
+		{{ csrf_field() }}
+		
+		<div class="c-form__field mb-4">
+			<label for="email">{{trans('auth.email_label')}}</label>
+			@if( isset($errors) && $errors->has('email') )
+				<span class="field-error">{{ implode(",", isset($errors) && $errors->get('email') ? $errors->get('email') : [])  }}</span>
+			@endif
+			<input type="email" class="c-form__input c-form__input--larger" required id="email" tabindex="1" name="email" value="" autofocus />
+		</div>
 
-</div>
+		<div class="c-form__field mb-8">
+			<button type="submit" class="button button--primary" tabindex="2">{{trans('passwords.forgot.submit')}}</button>
+		</div>
+
+		<div class="mb-4">
+			{{ trans('auth.have_login_and_password') }}&nbsp;<a tabindex="3" class="" href="{{ route('register') }}">
+				{{ trans('auth.login') }}
+			</a>
+		</div>
+
+	</form>
 
 @stop
