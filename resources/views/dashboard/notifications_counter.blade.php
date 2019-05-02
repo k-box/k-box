7259026ab8b551ec6d40cdc6f9156bbfa4ae773b
Alessio <alessio.vertemati@gmail.com>
2019-05-02 15:04:27 +0200

    Clean unused code

--- resources/views/dashboard/notifications_counter.blade.php
+++ resources/views/dashboard/notifications_counter.blade.php
@@ -1,12 +1,1 @@
 
-{{-- @if(isset($notices)) --}}
-
-{{-- <div class="notifications-counter">
-	
-	<span class="icon-social-white icon-social-white-ic_notifications_white_24dp"></span>
-
-	<div class="badge">5</div>
-
-</div> --}}
-
-{{-- @endif --}}
