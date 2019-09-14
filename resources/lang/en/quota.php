87283e0476ca44afa1bbd93e1d085b72867b2959
Alessio <alessio.vertemati@gmail.com>
2019-09-14 14:22:43 +0200

    Show quota information inside user's profile

--- resources/lang/en/quota.php
+++ resources/lang/en/quota.php
@@ -14,6 +14,9 @@ return [
     'exceeded' => 'You have exceeded the storage quota assigned',
     'not_enough_free_space' => 'The file you are trying to upload exceeds :quota quota limit. You need an additional :necessary_free_space to upload this file.',
 
+    'menu_label' => 'Storage',
+    'page_title' => 'Manage storage',
+
     'notifications' => [
         'limit' => [
             'subject' => 'Your K-Box account is approaching the quota limit',
@@ -23,6 +26,22 @@ return [
             'subject' => 'Your K-Box account is almost full',
             'text' => 'We are to inform you that your K-Box account as reached its storage limit (:quota).',
         ],
+    ],
+
+    'unlimited' => 'Unlimited',
+
+    'unlimited_label' => 'You have unlimited storage',
+
+
+    'threshold' => [
+        'section' => 'Used storage notification',
+        'hint' => 'Configure the percentage of used storage after which you would like to receive a notification',
+        'update_btn' => 'Update threshold',
+        
+        'acceptable_value' => 'minimum 5, maximum 98',
+
+        'updated' => 'Notification threshold changed to :threshold%.',
+        'not_updated_unlimited' => 'Notification threshold not changed as storage is unlimited.',
     ],
 
 ];
--- resources/views/quota/widget.blade.php
+++ resources/views/quota/widget.blade.php
@@ -3,11 +3,11 @@
     @unless ($unlimited)
         
         @component('components.progress', ['percentage' => $percentage ?? 0, 'height' => 'h-1'])
-            <p class="text-sm">
+            <p class="text-sm text-gray-700">
                 {{ trans('widgets.storage.title') }}
             </p>
             <p class="text-sm">
-                {{ trans('widgets.storage.used', ['used' => $used, 'total' => $total]) }}
+                <a href="{{ route('profile.storage.index') }}" class="no-underline text-gray-700">{{ trans('widgets.storage.used', ['used' => $used, 'total' => $total]) }}</a>
             </p>
         @endcomponent
     @endunless
