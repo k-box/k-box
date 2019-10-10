947ac743840b03659b449f4a2c7096892f75379c
Alessio <alessio.vertemati@gmail.com>
2019-10-10 12:34:35 +0200

    Clean cache when share are created.

--- app/Listeners/ShareCreatedHandler.php
+++ app/Listeners/ShareCreatedHandler.php
@@ -5,8 +5,10 @@ namespace KBox\Listeners;
 use KBox\Events\ShareCreated;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Cache;
 use Log;
 use KBox\Notifications\ShareCreatedNotification;
+use KBox\User;
 
 /**
  * Handler for the {@see KBox\Events\ShareCreated} event
@@ -37,9 +39,13 @@ class ShareCreatedHandler implements ShouldQueue
     {
         $to = $event->share->sharedwith;
 
-        if (is_a($to, \KBox\User::class)) {
+        if (is_a($to, User::class)) {
             Log::info('share created', compact('event'));
 
+            // clean the sidebar cache
+            Cache::forget('dms_shared_collections'.$to->id);
+
+            // notify to the user
             $to->notify(new ShareCreatedNotification($event->share));
         }
     }
