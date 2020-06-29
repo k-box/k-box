a3807da4e0d9c3b0c253e494d26d3b4202191cb2
Alessio <alessio.vertemati@gmail.com>
2020-06-29 19:17:03 +0200

    Code style

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,5 +69,4 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
-
 }
