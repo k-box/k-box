d69466b65ccfb2e683e34627d7874a02447f98eb
Alessio <alessio.vertemati@gmail.com>
2020-07-30 20:45:05 +0200

    Use inline-flex for checkbox component

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,4 +1,4 @@
-<label class="flex items-center">
+<label class="inline-flex items-center">
     <input type="checkbox" class="form-checkbox" name="{{ $name ?? 'checkbox' }}"  {{ isset($checked) && $checked ? 'checked' : '' }}>
     <span class="ml-2">{{ $slot }}</span>
 </label>
