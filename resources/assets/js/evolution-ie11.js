df78e11cd8b066a05353f8a77a1d2f711aaf5894
Alessio <alessio.vertemati@gmail.com>
2020-08-02 18:16:42 +0200

    Include dependencies in IE11 build

--- resources/assets/js/evolution-ie11.js
+++ resources/assets/js/evolution-ie11.js
@@ -6,3 +6,9 @@
  */
 
 import 'alpinejs/dist/alpine-ie11'
+
+import Dialog from "./components/dialog"
+import AsyncForm from "./components/asyncForm"
+
+window.Dialog = Dialog;
+window.AsyncForm = AsyncForm;
