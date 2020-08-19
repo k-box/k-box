3ebb46143a9ce1478223563c950acfabc77bf84c
Alessio <alessio.vertemati@gmail.com>
2020-08-19 09:57:25 +0200

    Correctly apply background color

--- resources/views/components/hero-image.blade.php
+++ resources/views/components/hero-image.blade.php
@@ -1,4 +1,4 @@
-<div class="hidden lg:block lg:min-h-screen lg:fixed lg:inset-y-0 lg:right-0 lg:w-1/2 {{ $fillColor ?? 'bg-gray-900' }}">
+<div class="hidden lg:block lg:min-h-screen lg:fixed lg:inset-y-0 lg:right-0 lg:w-1/2 @unless($fillColor) bg-gray-900 @endunless" @if ($fillColor) style="background-color: {{ $fillColor }}" @endif>
     @if ($hasPicture())
         <img class="w-full object-cover lg:w-full h-full" src="{{ $picture }}" alt="">
     @endif
