1b99a48f9096bd41bf319d70208aaa4c4216575f
Alessio <alessio.vertemati@gmail.com>
2020-11-14 20:50:17 +0100

    Add oauth identities configuration

---
+++ config/identities.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | OAuth Identity Providers
+    |--------------------------------------------------------------------------
+    |
+    | This file control which OAuth provider is enabled for user
+    | log in and registration. By default the feature is
+    | disabled. Set the providers value to a comma
+    | separated list of provider names to enable.
+    |
+    */
+
+    'providers' => env('KBOX_IDENTITIES_PROVIDERS', null),
+
+    'supported' => [
+        'gitlab',
+        'dropbpx',
+    ],
+
+];
