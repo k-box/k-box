00d5b8f43d821a4dcd09c65463194eaeeb583a56
Alessio <avvertix@users.noreply.github.com>
2020-11-24 19:10:15 +0100

    Allow to create new projects from the sidebar (#460)
    * Improve layout of empty message for projects

--- resources/views/groups/tree.blade.php
+++ resources/views/groups/tree.blade.php
@@ -2,7 +2,17 @@
 <div id="document-tree" class="tree-view">
 	@if(Auth::user()->can('create', \KBox\Project::class) || Auth::user()->can('viewAny', \KBox\Project::class))
 		@if(! ($private_groups->isEmpty() && config('dms.hide_projects_if_empty')) || Auth::user()->can('create', \KBox\Project::class))
-			<a href="{{ route('documents.projects.index') }}" class="navigation__item navigation__item--link @if(\Request::is('*projects')) navigation__item--current @endif"><strong>{{trans('projects.page_title')}}</strong></a>
+			<div class="flex items-center">
+				<a href="{{ route('documents.projects.index') }}" class="flex-grow navigation__item navigation__item--link @if(\Request::is('*projects')) navigation__item--current @endif"><strong>{{trans('projects.page_title')}}</strong></a>
+	
+				@can('create', \KBox\Project::class)
+					<div class="navigation__actions flex-grow-0 mr-2" x-data="{}">
+						<a href="{{route('projects.create')}}" class="navigation__action" title="{{trans('projects.new_button')}}">
+							@materialicon('content', 'add_circle_outline', 'btn-icon')
+						</a>
+					</div>
+				@endcan
+			</div>
 
 			<div class="tree-group">
 					
@@ -13,8 +23,12 @@
 						@include('groups.tree-item')
 						
 					@empty
+
+						<div class="empty empty--small">
 					
-						<p>{{trans('groups.collections.empty_private_msg')}}</p>
+							<p class="empty__description">{{trans('groups.collections.empty_private_msg')}}</p>
+
+						</div>
 						
 					@endforelse
 
