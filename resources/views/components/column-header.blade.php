7548b6989e1bbc990d3d11810d03398e3630f869
Alessio <alessio.vertemati@gmail.com>
2020-12-05 21:20:29 +0100

    Improve icon colors

--- resources/views/components/column-header.blade.php
+++ resources/views/components/column-header.blade.php
@@ -7,20 +7,20 @@
 
             <x-slot name="panel">
                 <ul class="text-black">
-                    <li><a class="no-underline flex p-2 -mx-2 mb-1 {{ $sort->current($key) && $sort->isAsc() ? 'text-accent-600' : 'text-black' }} hover:bg-blue-100 active:bg-blue-200 focus:bg-blue-100 focus:outline-none" href="{{ $sort->url(['sc' => $key, 'o' => \KBox\Sorter::ORDER_ASCENDING]) ?? '#' }}">
-                        @materialicon('navigation', 'arrow_downward', ['class' => 'fill-current text-gray-500 mr-2']) {{ trans("sort.directions.{$sort->type($key)}.a") }}
+                    <li><a class="no-underline flex p-2 -mx-2 mb-1 {{ $sort->current($key) && $sort->isAsc() ? 'text-accent-600' : 'text-gray-600' }} hover:bg-blue-100 active:bg-blue-200 focus:bg-blue-100 focus:outline-none" href="{{ $sort->url(['sc' => $key, 'o' => \KBox\Sorter::ORDER_ASCENDING]) ?? '#' }}">
+                        @materialicon('navigation', 'arrow_downward', 'fill-current mr-2') {{ trans("sort.directions.{$sort->type($key)}.a") }}
                     </a></li>
-                    <li><a class="no-underline flex p-2 -mx-2 mb-1 {{ $sort->current($key) && $sort->isDesc() ? 'text-accent-600' : 'text-black' }} hover:bg-blue-100 active:bg-blue-200 focus:bg-blue-100 focus:outline-none" href="{{ $sort->url(['sc' => $key, 'o' => \KBox\Sorter::ORDER_DESCENDING]) ?? '#' }}">
-                        @materialicon('navigation', 'arrow_upward', ['class' => 'fill-current text-gray-500 mr-2']) {{ trans("sort.directions.{$sort->type($key)}.d") }}
+                    <li><a class="no-underline flex p-2 -mx-2 mb-1 {{ $sort->current($key) && $sort->isDesc() ? 'text-accent-600' : 'text-gray-600' }} hover:bg-blue-100 active:bg-blue-200 focus:bg-blue-100 focus:outline-none" href="{{ $sort->url(['sc' => $key, 'o' => \KBox\Sorter::ORDER_DESCENDING]) ?? '#' }}">
+                        @materialicon('navigation', 'arrow_upward', 'fill-current mr-2') {{ trans("sort.directions.{$sort->type($key)}.d") }}
                     </a></li>
                 </ul>
             </x-slot>
         
             <div class="inline-flex items-center" title="{{ trans('sort.change_direction') }}">
                 @if($sort->current($key))
-                    @materialicon('navigation', $sort->isDesc() ? 'arrow_upward' : 'arrow_downward', ['class' => 'fill-current text-gray-400 w-4 h-4'])
+                    @materialicon('navigation', $sort->isDesc() ? 'arrow_upward' : 'arrow_downward', 'fill-current text-gray-400 w-4 h-4')
                 @endif
-                @materialicon('navigation', 'arrow_drop_down', ['class' => 'inline fill-current arrow', ':class' => "{ 'rotate-180': open }"])
+                @materialicon('navigation', 'arrow_drop_down', 'inline fill-current arrow', [':class' => "{ 'rotate-180': open }"])
             </div>
 
         </x-dropdown>
--- resources/views/components/sort.blade.php
+++ resources/views/components/sort.blade.php
@@ -4,11 +4,11 @@
     <div {{ $attributes->merge(['class' => 'relative']) }}  x-data="{ open: false }">
 
         <button type="button" @click="open = true" class="flex items-center button" title="{{ trans('sort.button') }}">
-            @materialicon('navigation', $sorter->isDesc() ? 'arrow_upward' : 'arrow_downward', ['class' => 'fill-current text-gray-400 w-4 h-4'])
+            @materialicon('navigation', $sorter->isDesc() ? 'arrow_upward' : 'arrow_downward', 'fill-current text-gray-400 w-4 h-4')
 
             {{ trans("sort.labels.{$sorter->field}") }}
             
-            @materialicon('navigation', 'arrow_drop_down', ['class' => 'inline fill-current arrow', ':class' => "{ 'rotate-180': open }"])
+            @materialicon('navigation', 'arrow_drop_down', 'inline fill-current arrow', [':class' => "{ 'rotate-180': open }"])
         </button>
 
         <div @click.away="open = false" x-show.transition="open" x-cloak class="absolute shadow-lg w-screen sm:w-56 p-2 mt-1 text-white bg-white rounded z-10 sm:right-0 }}">
@@ -17,18 +17,18 @@
                 @foreach ($sorter->sortables as $key => $opts)
 
                     <li class="flex items-center justify-center">
-                        <a class="no-underline p-2 {{ $sorter->current($key) && $sorter->isAsc() ? 'text-accent-600' : 'text-black' }} hover:bg-blue-100 active:bg-blue-200 focus:bg-blue-100 focus:outline-none" 
+                        <a class="no-underline p-2 {{ $sorter->current($key) && $sorter->isAsc() ? 'text-accent-600' : 'text-gray-600' }} hover:bg-blue-100 active:bg-blue-200 focus:bg-blue-100 focus:outline-none" 
                             title="{{ trans("sort.directions.{$sorter->type($key)}.a") }}"
                             href="{{ $sorter->url(['sc' => $key, 'o' => \KBox\Sorter::ORDER_ASCENDING]) }}">
-                            @materialicon('navigation', 'arrow_downward', ['class' => 'fill-current'])
+                            @materialicon('navigation', 'arrow_downward', 'fill-current')
                         </a>
                         <span class="flex-grow text-center no-underline {{ $sorter->current($key) ? 'text-accent-600' : 'text-black' }}">
                             {{ trans("sort.labels.{$key}") }}
                         </span>
-                        <a class="no-underline p-2 {{ $sorter->current($key) && $sorter->isDesc() ? 'text-accent-600' : 'text-black' }} hover:bg-blue-100 active:bg-blue-200 focus:bg-blue-100 focus:outline-none" 
+                        <a class="no-underline p-2 {{ $sorter->current($key) && $sorter->isDesc() ? 'text-accent-600' : 'text-gray-600' }} hover:bg-blue-100 active:bg-blue-200 focus:bg-blue-100 focus:outline-none" 
                             title="{{ trans("sort.directions.{$sorter->type($key)}.d") }}"
                             href="{{ $sorter->url(['sc' => $key, 'o' => \KBox\Sorter::ORDER_DESCENDING]) }}">
-                            @materialicon('navigation', 'arrow_upward', ['class' => 'fill-current'])
+                            @materialicon('navigation', 'arrow_upward', 'fill-current')
                         </a>
                     </li>
                         
