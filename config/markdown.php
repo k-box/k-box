0199a4697482347b218cda988657d222bfefff03
Alessio <alessio.vertemati@gmail.com>
2021-01-04 10:23:53 +0100

    Update commonmark

--- config/markdown.php
+++ config/markdown.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use League\CommonMark\Extension\ExternalLink\ExternalLinkExtension;
 use League\CommonMark\Extension\HeadingPermalink\HeadingPermalinkExtension;
 use League\CommonMark\Extension\DisallowedRawHTML\DisallowedRawHTMLExtension;
