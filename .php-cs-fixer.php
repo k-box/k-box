70d9f47e90ccff23e232f5554bd71a68b51def84
Alessio <alessio.vertemati@gmail.com>
2021-07-03 22:02:26 +0200

    Upgrade used php libraries

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -9,11 +9,11 @@ $fixers = [
     'elseif' => true,
     'encoding' => true,
     'single_blank_line_at_eof' => true,
-    'no_extra_consecutive_blank_lines' => true,
+    'no_extra_blank_lines' => true,
     'include' => true,
     'blank_line_after_namespace' => true,
     'not_operator_with_successor_space' => true,
-    'lowercase_constants' => true,
+    'constant_case' => true,
     'lowercase_keywords' => true,
     'array_syntax' => ['syntax' => 'short'],
     'no_unused_imports' => true
@@ -29,7 +29,7 @@ $finder = PhpCsFixer\Finder::create()
         __DIR__.'/tests',
     ]);
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setFinder($finder)
     ->setRules($fixers)
     ->setUsingCache(false);
